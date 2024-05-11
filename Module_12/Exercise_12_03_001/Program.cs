9706db037aba2bb1f53afa495942f91c2ec49a60
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-11 15:27:52 +0300

    test

---
+++ Module_12/Exercise_12_03_001/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Exercise_12_03_001
+{
+    internal class Program
+    {
+        static string SearchInArray(int[]array,int value)
+        {
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return $"Число найдено на позиции: {i + 1}\nДействий: {counter+1}";
+                }
+                counter++;
+            }
+            return $"Число не найдено\nДействий: {counter}";
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29 };
+           
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.Write("Введите число, которое необходимо найти: ");
+            var value=int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SearchInArray(array,value));
+                          
+        }
+    }
+}
