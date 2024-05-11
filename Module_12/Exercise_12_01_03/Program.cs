130f58cdb78fc11e264c237a66d31f594ca97826
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-11 12:36:26 +0300

    Exercise_12_01_03

---
+++ Module_12/Exercise_12_01_03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_12_01_03
+{
+    internal class Program
+    {
+        static void ArrayCount()
+        {
+            Console.Write("Сколько элементов будет в массиве: ");
+            int count=int.Parse(Console.ReadLine());
+            string[]array=new string[count];
+            for (int i=0; i<count; i++)
+            {
+                Console.Write($"Элемент №{i+1}: ");
+                array[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\n\nВсе элементы записаны!");
+        }
+        static void Main(string[] args)
+        {
+            ArrayCount();
+        }
+    }
+}
