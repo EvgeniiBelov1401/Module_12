95f3cbeaa40eb8b47024072f6e315de40884f1b6
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-11 12:25:14 +0300

    Exercise_12_01_02

---
+++ Module_12/Exercise_12_01_02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise_12_01_02
+{
+    internal class Program
+    {
+        static void Greeting()
+        {
+            Console.Write("Как Вас зовут?: ");
+            string name = Console.ReadLine();
+            string greetings = "Привет, " + name;
+            Console.WriteLine(greetings);
+        }
+        static void Main(string[] args)
+        {
+            Greeting();
+        }
+    }
+}
