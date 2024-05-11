8775fb6dbb1d1265dee01eea65e0a08c5e5bfc8b
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-11 12:43:06 +0300

    Exercise_12_01_04

---
+++ Module_12/Exercise_12_01_04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise_12_01_04
+{
+    internal class Program
+    {
+        static void CheckAge()
+        {
+            const int limitAge = 18;
+            Console.Write("Введите Ваш возраст: ");
+            int age=int.Parse(Console.ReadLine());
+
+            if (age >= limitAge)
+            {
+                Console.WriteLine("Вы успешно зпрегистрированы");
+            }
+            else
+            {
+                Console.WriteLine($"В регистрации отказано\nВаш возраст меньше {limitAge} лет");
+            }
+        }
+        static void Main(string[] args)
+        {
+            CheckAge();
+        }
+    }
+}
