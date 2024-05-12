3cde9c5c4566201b5deb3c43388d6ddfa0a18609
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-12 15:24:30 +0300

    Профилировщик производительности

---
+++ Module_12/Exercise_12_04_002/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+namespace Exercise_12_04_002
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Testing>();
+        }
+    }
+}
